cca3657e5d959ebc34fd713a6186f9511951f78d
Veronica Jung <veronicahjung@gmail.com>
2025-12-26 16:37:48 -0500

    first commit

---
+++ ackleyfunction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//definitions before function
+std::string throwaway;
+
+/*
+int somefunction()
+{
+
+}
+*/
+
+int main() {
+    std::cout << "Hello, I am an Ackley function minimizer" "\n";
+    std::cout << "First, how are you?" "\n";
+    std::cin >> throwaway;
+    std::cout << "Thanks for answering " << throwaway << std::endl;
+    std::cout << "Now let's actually begin :)" "\n";
+
+
+
+    return 0;
+}
