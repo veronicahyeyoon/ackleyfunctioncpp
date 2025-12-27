10a158da2537f55b6ce49e707d9bd9ee3ff38e9c
Veronica Jung <veronicahjung@gmail.com>
2025-12-26 21:23:19 -0500

    new vector for ackley

--- ignorepractice.cpp
+++ ignorepractice.cpp
@@ -10,14 +10,20 @@ double ackleyfunctioncalculator()
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dist(0.0, 20.0);
-    double j = dist(gen);
-    double k = dist(gen);
-    //std::vector<double> random_x_y_value_storage
-    std::cout << "this is the randomly generated j: " << j << std::endl;
-    std::cout << "this is the randomly generated k: " << k << std::endl;
+    double x = dist(gen);
+    double y = dist(gen);
+    std::vector<double> random_x_y_value_storage;
+    random_x_y_value_storage.push_back(x);
+    random_x_y_value_storage.push_back(y);
+    for (int i = 0; i < random_x_y_value_storage.size(); i++)
+    {
+        std::cout << random_x_y_value_storage[i]<< " ";
+    }
+    //std::cout << "this is the randomly generated x: " << x << std::endl;
+    //std::cout << "this is the randomly generated y: " << y << std::endl;
 
-    double a= -0.2*(sqrt((j*j+k*k)/2));
-    double b= (std::cos(2*std::acos(-1)*j)+std::cos(2*std::acos(1)*k))/2;
+    double a= -0.2*(sqrt((x*x+y*y)/2));
+    double b= (std::cos(2*std::acos(-1)*x)+std::cos(2*std::acos(1)*y))/2;
     double z = -20*std::exp(a)-std::exp(b)+20+std::exp(1);
 
     return z;
@@ -29,9 +35,10 @@ int main()
         double result = ackleyfunctioncalculator();
         std::vector<double> computed_z_value_storage;
         computed_z_value_storage.push_back(result);
+        std::cout << "this is the result: "<< result << std::endl;
+
     } 
 
-    std::cout << "this is the result: "<< result << std::endl;
 
     /*
     for (int i = 0; i < computed_z_value_storage.size(); i++)
